1c199c22625025581ae7882209b77c4f4b3b32da
Eduard Khiaev <eddietheone1984@gmail.com>
2023-03-23 11:19:08 +0200

    query_oui

--- OUI/query_oui/main.go
+++ OUI/query_oui/main.go
@@ -12,6 +12,10 @@ import (
 )
 
 func main() {
+	// Parse the command-line arguments
+	orgFlag := flag.Bool("organization", false, "Query by organization")
+	flag.Parse()
+
 	// Open the SQLite database
 	db, err := sql.Open("sqlite3", "oui.db")
 	if err != nil {
@@ -21,33 +25,37 @@ func main() {
 
 	// Prepare the SQL statement for querying the data
 	var stmt *sql.Stmt
-	var sqlQuery string
-
-	// Check if the organization flag is set
-	var organizationFlag = flag.Bool("organization", false, "Query by organization")
-	flag.Parse()
-
-	if *organizationFlag {
-		sqlQuery = "SELECT * FROM oui WHERE organization LIKE ?"
+	var query string
+	if *orgFlag {
+		stmt, err = db.Prepare("SELECT * FROM oui WHERE organization LIKE ?")
+		if err != nil {
+			log.Fatal(err)
+		}
+		query = strings.Join(flag.Args(), " ")
 	} else {
-		sqlQuery = "SELECT * FROM oui WHERE oui LIKE ?"
-	}
+		stmt, err = db.Prepare("SELECT * FROM oui WHERE oui LIKE ?")
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Get the MAC address from the command-line arguments
+		if len(flag.Args()) != 1 {
+			log.Fatal("Usage: go run query_mac_address.go <mac_address> | [-organization <organization>]")
+		}
+		macAddress := flag.Args()[0]
 
-	stmt, err = db.Prepare(sqlQuery)
-	if err != nil {
-		log.Fatal(err)
+		// Remove any non-alphanumeric characters from the MAC address
+		re := regexp.MustCompile(`[^0-9A-Fa-f]`)
+		macAddress = re.ReplaceAllString(macAddress, "")
+
+		// Convert the MAC address to lowercase
+		macAddress = strings.ToLower(macAddress)
+
+		// Extract the OUI from the MAC address
+		oui := macAddress[0:6]
+
+		query = oui + "%"
 	}
 	defer stmt.Close()
-
-	// Get the query string from the command-line arguments
-	if len(flag.Args()) != 1 {
-		log.Fatal("Usage: go run query_mac_address.go [--organization] <query>")
-	}
-	query := strings.ToLower(flag.Arg(0))
-
-	// Remove any non-alphanumeric characters from the query
-	re := regexp.MustCompile(`[^0-9A-Za-z ]`)
-	query = re.ReplaceAllString(query, "")
 
 	// Execute the SQL statement with the query string as the parameter
 	rows, err := stmt.Query("%" + query + "%")
@@ -56,7 +64,8 @@ func main() {
 	}
 	defer rows.Close()
 
-	// Loop over the rows and print the data
+	// Print the result
+	found := false
 	for rows.Next() {
 		var id int
 		var oui string
@@ -68,7 +77,19 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("OUI:\t\t%s\nOrganization:\t%s\nAddress:\t%s\nCity:\t\t%s\nCountry:\t%s\n\n", strings.ToUpper(oui), organization, address, city, country)
+		if *orgFlag {
+			fmt.Printf("OUI:\t\t%s\nOrganization:\t%s\nAddress:\t%s\nCity:\t\t%s\nCountry:\t%s\n", oui, organization, address, city, country)
+		} else {
+			fmt.Printf("Organization:\t%s\nAddress:\t%s\nCity:\t\t%s\nCountry:\t%s\nOUI:\t\t%s\n", organization, address, city, country, strings.ToUpper(oui))
+		}
+		found = true
+	}
+	if !found {
+		if *orgFlag {
+			fmt.Printf("No results found for organization: %s\n", query)
+		} else {
+			fmt.Printf("No results found for MAC address: %s\n", query)
+		}
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
