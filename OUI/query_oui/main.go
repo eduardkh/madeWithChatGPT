4c78fd5b9bb977e55f474923c2f15f87261d697b
Eduard Khiaev <eddietheone1984@gmail.com>
2023-03-23 10:51:56 +0200

    oui query start over

--- OUI/query_oui/main.go
+++ OUI/query_oui/main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
-	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -20,60 +18,34 @@ func main() {
 	defer db.Close()
 
 	// Prepare the SQL statement for querying the data
-	stmt, err := db.Prepare("SELECT * FROM oui WHERE oui LIKE ?")
+	stmt, err := db.Prepare("SELECT oui, organization FROM oui WHERE oui LIKE ? OR organization LIKE ?")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 
-	// Get the MAC address from the command-line arguments
+	// Get the query string from the command-line arguments
 	if len(os.Args) != 2 {
-		log.Fatal("Usage: go run query_mac_address.go <mac_address>")
+		log.Fatal("Usage: go run query_oui.go <query_string>")
 	}
-	macAddress := os.Args[1]
+	query := "%" + os.Args[1] + "%"
 
-	// Remove any non-alphanumeric characters from the MAC address
-	re := regexp.MustCompile(`[^0-9A-Fa-f]`)
-	macAddress = re.ReplaceAllString(macAddress, "")
-
-	// Convert the MAC address to lowercase
-	macAddress = strings.ToLower(macAddress)
-
-	// Check if the MAC address is valid
-	if len(macAddress) < 1 || len(macAddress) > 12 {
-		log.Fatal("Invalid MAC address")
-	}
-
-	// Extract the OUI from the MAC address
-	oui := macAddress[0:1]
-
-	// Execute the SQL statement with the OUI as the parameter
-	rows, err := stmt.Query(oui + "%")
+	// Execute the SQL statement with the query string as the parameter
+	rows, err := stmt.Query(query, query)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
-	// Print the result
-	found := false
+	// Loop over the rows and print the OUI and organization
 	for rows.Next() {
-		var id int
-		var organization string
-		var address string
-		var city string
-		var country string
 		var oui string
-		err := rows.Scan(&id, &oui, &organization, &address, &city, &country)
+		var organization string
+		err := rows.Scan(&oui, &organization)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if strings.HasPrefix(strings.ToLower(oui), oui) {
-			fmt.Printf("Organization:\t%s\nAddress:\t%s\nCity:\t\t%s\nCountry:\t%s\nOUI:\t\t%s\n", organization, address, city, country, strings.ToUpper(oui))
-			found = true
-		}
-	}
-	if !found {
-		fmt.Printf("No results found for %s\n", macAddress)
+		fmt.Printf("%s\t%s\n", oui, organization)
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
