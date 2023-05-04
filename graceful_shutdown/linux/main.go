c09382b5cce215e43227bb4f499c783fd0553de7
Eduard Khiaev <eddietheone1984@gmail.com>
2023-05-04 12:16:21 +0300

    graceful shutdown

---
+++ graceful_shutdown/linux/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go name")
+		os.Exit(1)
+	}
+	name := os.Args[1]
+	fmt.Printf("Hello, %s!\n", name)
+
+	// Signal handling code
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("Shutting down gracefully...")
+}
