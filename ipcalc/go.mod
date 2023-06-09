e69e956612768099c31dc1f9b7ffd65db8554353
Eduard Khiaev <eddietheone1984@gmail.com>
2023-06-09 09:46:14 +0300

    fix Usable IP Range

--- ipcalc/go.mod
+++ ipcalc/go.mod
@@ -1,3 +1,5 @@
 module ipcalc
 
 go 1.20
+
+require github.com/brotherpowers/ipsubnet v0.0.0-20170914094241-30bc98f0a5b1
---
+++ ipcalc/ipcalc.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/brotherpowers/ipsubnet"
+)
+
+func nextIPAddress(ip string) string {
+	parsedIP := net.ParseIP(ip)
+	for i := len(parsedIP) - 1; i >= 0; i-- {
+		if parsedIP[i] < 255 {
+			parsedIP[i]++
+			break
+		} else {
+			parsedIP[i] = 0
+		}
+	}
+	return parsedIP.String()
+}
+
+func previousIPAddress(ip string) string {
+	parsedIP := net.ParseIP(ip)
+	for i := len(parsedIP) - 1; i >= 0; i-- {
+		if parsedIP[i] > 0 {
+			parsedIP[i]--
+			break
+		} else {
+			parsedIP[i] = 255
+		}
+	}
+	return parsedIP.String()
+}
+
+func main() {
+	// Check if IP address and subnet mask/CIDR are provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: <IP>/<CIDR> or <IP> <Subnet Mask>")
+		os.Exit(1)
+	}
+
+	var ip string
+	var subnet int
+
+	// Check if CIDR notation is used
+	if strings.Contains(os.Args[1], "/") {
+		// Split IP address and CIDR
+		ipCIDR := strings.Split(os.Args[1], "/")
+		ip = ipCIDR[0]
+		fmt.Sscanf(ipCIDR[1], "%d", &subnet)
+	} else {
+		// Assume IP address and subnet mask are provided separately
+		ip = os.Args[1]
+		mask := net.IPMask(net.ParseIP(os.Args[2]).To4())
+		_, subnet = mask.Size()
+	}
+
+	// Initialize the subnet calculator
+	sub := ipsubnet.SubnetCalculator(ip, subnet)
+
+	// Calculate and print the network information
+	fmt.Println("Network    :", sub.GetNetworkPortion())
+	fmt.Println("Broadcast  :", sub.GetBroadcastAddress())
+	IPRange := sub.GetIPAddressRange()
+	fmt.Printf("Usable IP Range : %s - %s\n", nextIPAddress(IPRange[0]), previousIPAddress(IPRange[1]))
+}
