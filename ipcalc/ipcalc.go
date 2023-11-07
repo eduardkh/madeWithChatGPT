a3d2ce8b10a0321c1edc1e4a23a9897f73a23c6d
Eduard Khiaev <eddietheone1984@gmail.com>
2023-11-07 15:00:55 +0200

    ipcalc.go

--- ipcalc/ipcalc.go
+++ ipcalc/ipcalc.go
@@ -4,91 +4,59 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
-
-	"github.com/brotherpowers/ipsubnet"
 )
 
-func nextIPAddress(ip string) string {
-	parsedIP := net.ParseIP(ip)
-	for i := len(parsedIP) - 1; i >= 0; i-- {
-		if parsedIP[i] < 255 {
-			parsedIP[i]++
-			break
-		} else {
-			parsedIP[i] = 0
-		}
-	}
-	return parsedIP.String()
-}
-
-func previousIPAddress(ip string) string {
-	parsedIP := net.ParseIP(ip)
-	for i := len(parsedIP) - 1; i >= 0; i-- {
-		if parsedIP[i] > 0 {
-			parsedIP[i]--
-			break
-		} else {
-			parsedIP[i] = 255
-		}
-	}
-	return parsedIP.String()
-}
-
 func main() {
-	// Check if IP address and subnet mask/CIDR are provided
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: <IP>/<CIDR> or <IP> <Subnet Mask>")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ipcalc <IP>/<CIDR>")
 		os.Exit(1)
 	}
 
-	var ip string
-	var subnet int
-
-	// Check if CIDR notation is used
-	if strings.Contains(os.Args[1], "/") {
-		// Split IP address and CIDR
-		ipCIDR := strings.Split(os.Args[1], "/")
-		ip = ipCIDR[0]
-		fmt.Sscanf(ipCIDR[1], "%d", &subnet)
-	} else {
-		// Assume IP address and subnet mask are provided separately
-		ip = os.Args[1]
-		mask := net.IPMask(net.ParseIP(os.Args[2]).To4())
-		_, subnet = mask.Size()
+	ip, ipnet, err := net.ParseCIDR(os.Args[1])
+	if err != nil {
+		fmt.Println("Please provide a valid IP address and subnet or CIDR.")
+		os.Exit(1)
 	}
 
-	// Initialize the subnet calculator
-	sub := ipsubnet.SubnetCalculator(ip, subnet)
-
-	IPRange := sub.GetIPAddressRange()
-	network := sub.GetNetworkPortion()
-	broadcast := sub.GetBroadcastAddress()
-
-	usableIPRange := ""
-	hostInSubnet := ""
-
-	switch subnet {
-	case 31:
-		usableIPRange = fmt.Sprintf("%s - %s (RFC 3021)", IPRange[0], IPRange[1])
-		network = "N/A"
-		broadcast = "N/A"
-		hostInSubnet = "2 (RFC 3021)"
-	case 32:
-		usableIPRange = fmt.Sprintf("%s (Single host IP)", IPRange[0])
-		network = "N/A"
-		broadcast = "N/A"
-		hostInSubnet = "1 (Single host IP)"
-	default:
-		usableIPRange = fmt.Sprintf("%s - %s", nextIPAddress(IPRange[0]), previousIPAddress(IPRange[1]))
-		hostInSubnet = fmt.Sprint(sub.GetNumberAddressableHosts())
+	mask := ipnet.Mask
+	network := ip.Mask(mask)
+	broadcast := net.IP(make([]byte, 4))
+	for i := range network {
+		broadcast[i] = network[i] | ^mask[i]
 	}
 
-	// Calculate and print the network information
-	fmt.Printf("Address         : %s/%d\n", ip, subnet)
-	fmt.Println("Network         :", network)
-	fmt.Println("Usable IP Range :", usableIPRange)
-	fmt.Println("Broadcast       :", broadcast)
-	fmt.Println("SubnetMask      :", sub.GetSubnetMask())
-	fmt.Println("Host in Subnet  :", hostInSubnet)
+	ones, _ := ipnet.Mask.Size()
+	hostBits := 32 - ones
+	hosts := (1 << hostBits) - 2 // Subtract network and broadcast addresses
+
+	fmt.Printf("Address:     %s\n", ip)
+	fmt.Printf("Network:     %s\n", network)
+	fmt.Printf("Broadcast:   %s\n", broadcast)
+	fmt.Printf("Host Range:  %s - %s\n", nextIP(network), previousIP(broadcast))
+	fmt.Printf("Subnet Mask: %s\n", net.IP(mask).String())
+	fmt.Printf("Host Number: %d\n", hosts)
+}
+
+func nextIP(ip net.IP) net.IP {
+	next := net.IP(make([]byte, len(ip)))
+	copy(next, ip)
+	for j := len(next) - 1; j >= 0; j-- {
+		next[j]++
+		if next[j] > 0 {
+			break
+		}
+	}
+	return next
+}
+
+func previousIP(ip net.IP) net.IP {
+	previous := net.IP(make([]byte, len(ip)))
+	copy(previous, ip)
+	for j := len(previous) - 1; j >= 0; j-- {
+		previous[j]--
+		if previous[j] < 255 {
+			break
+		}
+	}
+	return previous
 }
