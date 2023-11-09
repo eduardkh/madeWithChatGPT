12cfd7299ba824f6a1fe8c80b9734880f439647c
Eduard Khiaev <eddietheone1984@gmail.com>
2023-11-09 17:22:36 +0200

    oui download_and_save

--- OUI/download_and_save/main.go
+++ OUI/download_and_save/main.go
@@ -1,76 +1,65 @@
 package main
 
 import (
-	"database/sql"
-	"io/ioutil"
-	"log"
+	"fmt"
+	"io"
 	"net/http"
-	"regexp"
-	"strings"
-
-	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	// Download the contents of the OUI file
-	resp, err := http.Get("https://standards-oui.ieee.org/oui/oui.txt")
+	err := DownloadOUIFile()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error:", err)
+	}
+}
+
+// DownloadOUIFile downloads the OUI CSV file from the IEEE website and stores it in the local application data directory.
+func DownloadOUIFile() error {
+	// Determine the local application data directory.
+	appDataDir, err := os.UserConfigDir()
+	if err != nil {
+		return fmt.Errorf("error getting user config directory: %w", err)
+	}
+
+	// Create a subdirectory for your app.
+	appDataDir = filepath.Join(appDataDir, "MyOUIApp")
+	if err := os.MkdirAll(appDataDir, 0755); err != nil {
+		return fmt.Errorf("error creating application data directory: %w", err)
+	}
+
+	// Define the URL and the local file path.
+	ouiURL := "http://standards-oui.ieee.org/oui/oui.csv"
+	localFilePath := filepath.Join(appDataDir, "oui.csv")
+
+	// Download the OUI file.
+	err = downloadFile(localFilePath, ouiURL)
+	if err != nil {
+		return fmt.Errorf("error downloading the OUI file: %w", err)
+	}
+
+	fmt.Printf("OUI file downloaded successfully to %s\n", localFilePath)
+	return nil
+}
+
+// downloadFile downloads the file from the given URL to the given local path.
+func downloadFile(filepath string, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
 	}
 	defer resp.Body.Close()
 
-	// Read the contents of the response
-	fileContents, err := ioutil.ReadAll(resp.Body)
+	// Create the file
+	out, err := os.Create(filepath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	defer out.Close()
 
-	// Compile the regular expression
-	re := regexp.MustCompile(`([0-9A-Fa-f]{6})\s+\(base 16\)\s+(.*)\n\s+(.*)\n\s+(.*)\n\s+(.*)\n\s`)
-
-	// Find all the matches of the regular expression
-	matches := re.FindAllStringSubmatch(string(fileContents), -1)
-
-	// Open the SQLite database
-	db, err := sql.Open("sqlite3", "oui.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Create the OUI table if it doesn't exist
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS oui (
-        id INTEGER PRIMARY KEY,
-        oui TEXT NOT NULL,
-        organization TEXT NOT NULL,
-        address TEXT NOT NULL,
-        city TEXT NOT NULL,
-        country TEXT NOT NULL
-    )`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Prepare the SQL statement for inserting the data
-	stmt, err := db.Prepare("INSERT INTO oui(oui, organization, address, city, country) VALUES(?, ?, ?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	// Loop over the matches and insert the data into the database
-	for _, match := range matches {
-		// Extract the fields from the matching groups
-		oui := strings.ToLower(strings.ReplaceAll(match[1], "-", ""))
-		organization := strings.TrimSpace(match[2])
-		address := strings.TrimSpace(match[3])
-		city := strings.TrimSpace(match[4])
-		country := strings.TrimSpace(match[5])
-
-		// Execute the SQL statement to insert the data
-		_, err := stmt.Exec(oui, organization, address, city, country)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
