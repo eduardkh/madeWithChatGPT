0384bd49eec309d7895037f3828c44891d600659
Eduard Khiaev <eddietheone1984@gmail.com>
2024-02-01 16:42:30 +0200

    "jq" friendly keys

--- python_crt.sh/main.py
+++ python_crt.sh/main.py
@@ -6,16 +6,22 @@ from io import StringIO
 url = "https://crt.sh/?q=crt.sh"
 response = requests.get(url)
 
+
+def format_key(key):
+    # Replace spaces and special characters with underscores
+    return key.replace(" ", "_").replace(".", "_").lower()
+
+
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find_all('table')[1]  # Correct table index
+    table = soup.find_all('table')[1]
 
     # Wrap the HTML in StringIO
     html_content = StringIO(str(table))
-    df = pd.read_html(html_content)[1]  # Correct table reference
+    df = pd.read_html(html_content)[1]
 
-    # Convert column names to string and remove the arrow symbol
-    df.columns = [str(col).replace(' ⇧', '') for col in df.columns]
+    # Format column names
+    df.columns = [format_key(col) for col in df.columns]
 
     # Save to JSON as a list of objects
     df.to_json("output.json", orient="records", lines=False)
